9774d4c1cf9674548032120288ea5196c3aecb51
Yash Rajput <yash92726@gmail.com>
2026-08-10 07:57:24 +0530

    Update MarkupCanvas.kt

--- app/src/main/kotlin/com/yash/privategallery/ui/editor/MarkupCanvas.kt
+++ app/src/main/kotlin/com/yash/privategallery/ui/editor/MarkupCanvas.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
 import com.yash.privategallery.domain.model.EditOperation
 import com.yash.privategallery.domain.model.ShapeType
 
